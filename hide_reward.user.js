7b86e250b27dec682e0c80158348d5bc7f398862
Alexei Mereniuc <124469192+AlexeiMereniuc@users.noreply.github.com>
2026-01-13 19:42:46 +0200

    Create hide_reward.user.js

---
+++ hide_reward.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name Скрыть награду за клановую медаль
+// @version 1.0
+// @description
+// @author Винни Пух
+// @match http://www.mafia-rules.net/*
+// @match https://www.mafia-rules.net/*
+// @grant none
+// @license MIT
+
+// ==/UserScript==
+
+document.getElementById('pp_duel_season_award').remove()
